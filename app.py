a8040da1527074e54e5c4236bfcb5cd324630139
Jesusvivara <65578168+Jesusvivara@users.noreply.github.com>
2022-03-31 09:25:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-flask import Flask, request
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 from datetime import datetime
